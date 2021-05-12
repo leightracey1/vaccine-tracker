474af870b0ba6830cb9218c5b28588ef314deaaf
leightracey1 <ltracey203@gmail.com>
2021-05-12 13:29:17 -0400

    clean up code

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config(); 
 
-const cron = require("node-cron")
-const axios = require("axios")
-const cheerio = require("cheerio")
+const cron = require("node-cron");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const accountSid = process.env.accountSid;
 const authToken = process.env.authToken;
 const client = require('twilio')(accountSid, authToken);
@@ -16,10 +16,10 @@ cron.schedule("*/1 * * * *", () => {
 getHackensackAppt()
 function getHackensackAppt() {
     axios.get("https://www.hackensackmeridianhealth.org/covid19/").then(function(response){
-    var $ = cheerio.load(response.data); 
+    const $ = cheerio.load(response.data); 
 
     if (!$("#pagealert2").text() || $("#pagealert2").text() != "All appointments currently are full. We hope to schedule again as more vaccines are received. Thank you for your patience. Please continue to check back.") {
-        alertOfOpening("Hackensack Meridian Health")
+        alertOfOpening("Hackensack Meridian Health");
     }
 })
 }
